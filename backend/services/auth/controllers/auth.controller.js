81f4ab459e7f1a066fe5de0d44ec40b6b060bda3
Anmol1578 <anmol10562@gmail.com>
2026-07-09 13:59:09 +0530

    add Redis integration for session management; implement login and logout functionality

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import { app } from "../config/firebase.js";
 import User from "../models/user.model.js";
 import { createConnection } from "mongoose";
 import crypto from "crypto";
+import redis from "../../../shared/redis/redis.js";
 
 export const login = async (req, res) => {
   try {
@@ -22,6 +23,18 @@ export const login = async (req, res) => {
     // For security, you should generate a session token and set it as an HTTP-only cookie. This will help prevent XSS attacks and ensure that the session is secure.
 
     const sessionToken = crypto.randomUUID();
+    await redis.set(
+      `session:${sessionToken}`,
+      JSON.stringify({
+        userId: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+      }),
+      "EX",
+      7 * 24 * 60 * 60,
+    ); // Set session to expire in 7 days
+
     res.cookie("session", sessionToken, {
       httpOnly: true,
       secure: false,
@@ -34,3 +47,15 @@ export const login = async (req, res) => {
     res.status(500).json({ message: `Error logging in: ${error.message}` });
   }
 };
+
+export const logout = async (req, res) => { 
+try { 
+  const { session } = req.cookies?.session;
+  await redis.del(`session:${session}`);
+  res.clearCookie("session");
+  return res.status(200).json({ message: "Logout successful" });
+
+} catch (error) {
+  res.status(500).json({ message: `Error logging out: ${error.message}` });
+} 
+}
