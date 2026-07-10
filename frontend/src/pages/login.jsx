b973a50d546cecd2e0cd262342f6c7520abb7989
Anmol1578 <anmol10562@gmail.com>
2026-07-10 12:52:07 +0530

    add LandingPage component; implement Google login functionality and update package dependencies

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,36 @@
+import { signInWithPopup } from "firebase/auth";
+import React from "react";
+import { auth, googleProvider } from "../../utils/firebase";
+import api from "../../utils/axios";
+
+function LandingPage() {
+  const handleLogin = async (token) => {
+    try {
+      const data = await api.post("/auth/login", { token });
+      console.log("Login response:", data);
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  const googleLogin = async () => {
+    try {
+      const data = await signInWithPopup(auth, googleProvider);
+
+      const token = await data.user.getIdToken();
+      console.log("Token:", token);
+      await handleLogin(token);
+
+      console.log("Google login successful:", data);
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  return <div className="h-screen flex bg-[#">
+
+
+  </div>;
+}
+
+export default LandingPage;
