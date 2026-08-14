e253bef67462eeed9c64c9f779072a82b4b72eef
Anmol1578 <anmol10562@gmail.com>
2026-08-14 23:49:02 +0530

    Update maxTokens for openrouter model to 4000

--- backend/services/agent/config/llmmodels.js
+++ backend/services/agent/config/llmmodels.js
@@ -37,7 +37,7 @@ const gemini = new ChatGoogleGenerativeAI({
 const openrouter = new ChatOpenRouter({
   model: "deepseek/deepseek-chat",
   temperature: 0,
-  maxTokens:  8000,
+  maxTokens:  4000,
 });
 
 export const getModel = (agent) => {
