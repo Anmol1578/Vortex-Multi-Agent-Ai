f3f4ab55453bf6f812321b4ccb763351c4a0dc39
Anmol1578 <anmol10562@gmail.com>
2026-09-03 17:07:52 +0530

    Update cookie settings for session management: change secure to true and sameSite to none for deployment compatibility

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -51,8 +51,8 @@ export const login = async (req, res) => {
 
     res.cookie("session", sessionToken, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true, //  FALSE TO TRUE CHANGE FOR RENDER DEPLOYMENT
+      sameSite: "none", //  STRICT TO NONE CHANGE FOR RENDER DEPLOYMENT
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -95,8 +95,8 @@ export const logout = async (req, res) => {
 
     res.clearCookie("session", {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true, // false to true CHANGE FOR RENDER DEPLOYMENT
+      sameSite: "none",   // strict to none CHANGE FOR RENDER DEPLOYMENT
     });
 
     return res.status(200).json({
